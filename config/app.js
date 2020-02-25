8f1d52768abf3e0bac14f0da2906c259daec17e2
fernik <nikejoelmadrid@gmail.com>
2020-02-25 13:30:54 -0500

    configuracion de la aplicacion

--- config/app.js
+++ config/app.js
@@ -1,8 +1,46 @@
+// url base for location view, database
 export const base_url = location.origin;
 
+// location assets
 export const assets_location= {
     js: '/assets/js/',
     css: '/assets/css/'
 };
 
-export const append_view = document.querySelector('#view');
+/**
+ * configuration view
+ * @typedef cview
+ * @type {object}
+ * @property {string} path - url view
+ * @property {string} ext - use extension
+ * @property {HTMLElement} join - when add view
+ */
+export const cview = {
+    path: (base_url +'/views/'),
+
+    ext: '.html',
+
+    join: document.querySelector('#content')
+}
+
+// location db
+const pdb = base_url +'/data/db';
+
+/**
+ * json format database configuration
+ * @typedef cdb
+ * @type {object}
+ * @property {string} path - folder where save data
+ * @property {string} connection - type connection [stream|url]
+ * @property {string} root - main db - use administrator panel
+ * @property {string} ability_save_local_data - administrator panel for data storage
+ */
+export const cdb = {
+    path: pdb,
+
+    connection: 'stream',
+
+    root: (pdb + '/def.json'),
+
+    ability_save_local_data: true
+}
