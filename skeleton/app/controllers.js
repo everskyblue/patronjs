aabfa8cee60fdddffec08b453329294af498da7b
fernik <nikejoelmadrid@gmail.com>
2020-11-12 22:34:43 -0500

    movido al directorio principal

--- skeleton/app/controllers.js
+++
@@ -1,3 +0,0 @@
-export {default as IndexController} from "./controller/index-controller.js";
-export {default as ContactController} from "./controller/contact-controller.js";
-export {default as ArticleController} from "./controller/articles-controller.js";
--- skeleton/public/readme.txt
+++
@@ -1,2 +0,0 @@
-This template (404, 500) was made by Colorlib (https://colorlib.com)
-Please visit our website for more awesome templates, themes and tools. 
