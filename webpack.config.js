b054b3f84029aa7fa768856981201ad448d24686
fernik <nikejoelmadrid@gmail.com>
2020-11-20 01:15:48 -0500

    añadido submodulos

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, 'lib'),
+    entry: path.resolve(__dirname, 'patron-core/global.js'),
     output: {
-        path: path.resolve(__dirname, "bundle"),
+        path: path.resolve(__dirname, "patron-core/bundle"),
         filename: "patron.bundle.js",
         libraryTarget: "umd"
     },
@@ -11,8 +11,14 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_module/,
-                use: ['babel-loader']
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ["@babel/plugin-transform-async-to-generator", "@babel/plugin-transform-runtime"]
+                    }
+                }
             }
         ]
     }
